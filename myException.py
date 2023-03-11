2c60e3a0988d9c8323c8978d062f99bb399354e4
mirko <mirko_antolini@libero.it>
2023-03-11 02:15:31 +0100

    updated myException.py file

--- myException.py
+++ myException.py
@@ -1,94 +1,49 @@
+import json
+
 class MyParserException(Exception):
 
     def __init__(self, msg):
         
         if msg:
-            if isinstance(msg, str):
-                self.message = msg
-            else:
-                self.message = None
-        else:
-            self.message = None
+            
+            if isinstance(msg, str): self.message = msg
 
-    def __str__(self):
+            elif isinstance(msg, dict) or isinstance(msg, list): self.message = "\n" + json.dumps(msg, indent=4)
 
-        if self.message:
-            return self.message
-        else:
-            return 'MyParserException has been raised'
+            else: self.message = None
+
+        else: self.message = None
+
+    def __str__(self): return self.message if self.message else "MyParserException has been raised"
 
 class MySyntaxException(Exception):
 
     def __init__(self, msg):
         
         if msg:
-            if isinstance(msg, str):
-                self.message = msg
-            else:
-                self.message = None
-        else:
-            self.message = None
+            
+            if isinstance(msg, str): self.message = msg
 
-    def __str__(self):
+            elif isinstance(msg, dict) or isinstance(msg, list): self.message = "\n" + json.dumps(msg, indent=4)
 
-        if self.message:
-            return self.message
-        else:
-            return 'MySyntaxException has been raised'
+            else: self.message = None
+
+        else: self.message = None
+
+    def __str__(self): return self.message if self.message else "MySyntaxException has been raised"
             
 class MyTypeException(Exception):
 
     def __init__(self, msg):
         
         if msg:
-            if isinstance(msg, str):
-                self.message = msg
-            else:
-                self.message = None
-        else:
-            self.message = None
+            
+            if isinstance(msg, str): self.message = msg
 
-    def __str__(self):
+            elif isinstance(msg, dict) or isinstance(msg, list): self.message = "\n" + json.dumps(msg, indent=4)
 
-        if self.message:
-            return self.message
-        else:
-            return 'MyTypeException has been raised'
+            else: self.message = None
 
-class MyDatabaseException(Exception):
+        else: self.message = None
 
-    def __init__(self, msg):
-        
-        if msg:
-            if isinstance(msg, str):
-                self.message = msg
-            else:
-                self.message = None
-        else:
-            self.message = None
-
-    def __str__(self):
-
-        if self.message:
-            return self.message
-        else:
-            return 'MyDatabaseException has been raised'
-
-class MyMailException(Exception):
-
-    def __init__(self, msg):
-        
-        if msg:
-            if isinstance(msg, str):
-                self.message = msg
-            else:
-                self.message = None
-        else:
-            self.message = None
-
-    def __str__(self):
-
-        if self.message:
-            return self.message
-        else:
-            return 'MyMailException has been raised'
+    def __str__(self): return self.message if self.message else "MyTypeException has been raised"
